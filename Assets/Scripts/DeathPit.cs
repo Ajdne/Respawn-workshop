fdbf54f229a0d73d94c9bd929d516ecda4ac1e34
Srđan Radujko <62765673+Ajdne@users.noreply.github.com>
2022-06-06 14:09:46 +0200

    Quick fix

--- Assets/Scripts/DeathPit.cs
+++ Assets/Scripts/DeathPit.cs
@@ -8,6 +8,16 @@ public class DeathPit : MonoBehaviour
     {
         
         Destroy(other.gameObject);
+        
+        if(other.gameObject.layer == 6) // if other game object is a red ball
+        {
+            GameManager.Instance.blueBallActive = false;
+        }
+
+        if(other.gameObject.layer == 7) // if other game object is a blue ball
+        {
+            GameManager.Instance.blueBallActive = false;
+        }
 
         if(other.gameObject.layer == 9) // if other is the red player
         {
